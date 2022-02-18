859eb32c1878bb405a7819aaa93acf827ec72b1c
samrondot <89955662+samrondot@users.noreply.github.com>
2022-02-18 16:07:14 -0500

    Start

---
+++ src/test/java/com/tasklist/TaskList/TaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasklist.TaskList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
