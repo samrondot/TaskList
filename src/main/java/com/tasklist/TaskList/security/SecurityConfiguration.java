a7c72d2025a40d20624f09fb5def993122c791a1
samrondot <89955662+samrondot@users.noreply.github.com>
2022-02-22 15:59:18 -0500

    Update

--- src/main/java/com/tasklist/TaskList/security/SecurityConfiguration.java
+++ src/main/java/com/tasklist/TaskList/security/SecurityConfiguration.java
@@ -33,13 +33,15 @@ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http
-				.csrf().disable()
 				.authorizeRequests()
 				.antMatchers("/admin/**").hasAnyRole("ADMIN")
+				.antMatchers("/register").permitAll()
 				.anyRequest().hasAnyRole("USER").and()
 				.formLogin()
 					.loginPage("/login")
 					.defaultSuccessUrl("/dashboard")
 					.permitAll();
+			
+			
 	}
 }
--- src/main/java/com/tasklist/TaskList/web/DashBoardController.java
+++ src/main/java/com/tasklist/TaskList/web/DashBoardController.java
@@ -1,12 +1,17 @@
 package com.tasklist.TaskList.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tasklist.TaskList.domain.User;
 
 @Controller
 public class DashBoardController {
 	@GetMapping("/dashboard")
-	public String getDashboard() {
+	public String getDashboard(@AuthenticationPrincipal User user, ModelMap model) {
+		model.put("user", user);	
 		return "dashboard";
 	}
 }
